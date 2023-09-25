8086e169f782c9a35e828541381ba56cf8ed187b
EllenPinheiro <ellen.pinheiro2@estudante.ifms.edu.br>
2023-09-25 08:22:22 -0400

    instalando a biblioteca react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
+import Filme from './componentes/filme/Filme'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header/>
-      <Footer/> 
+      <Footer/>
+      <Filme/> 
     </div>
 )
 }
